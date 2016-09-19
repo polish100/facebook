29e92ac65222bb24116d93b901c71d4bd455ce2e
ikeda <ikebright@gmail.com>
2016-09-20 01:52:29 +0900

    design_2

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,29 @@
 
-#   email = Faker::Internet.email
-#   password = "password"
-#   name = Faker::Name.name
-
-# 100.times do |n|
-#   User.create!(email: email,
-#                password: password,
-#                password_confirmation: password,
-#                name: name,
-#                )
-# end
 
 
+30.times do |n|
+  email = Faker::Internet.email
+  password = "password"
+  name = Faker::Name.name
+  uid = Faker::Number.number(10) #=> "1968353479"
+  User.create!(email: email,
+               password: password,
+               password_confirmation: password,
+               name: name,
+               uid: uid,
+               )
+end
 
-n = 203
-while n <= 302
+
+
+n = 1
+while n <= 30
   title = Faker::Book.title
   content = Faker::StarWars.quote
   Topic.create!(
     title: title,
     content: content,
-    user_id: n
+    user_id: 31
   )
   n += 1
 end
